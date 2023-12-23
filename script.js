e941993293f312c10702f1f66b49f29432c970b2
8FishBait <138678048+8fish8bait@users.noreply.github.com>
2023-12-23 22:00:14 +0100

    Ändrade allt

---
+++ script.js
@@ -0,0 +1,16 @@
+var time = new Date("December 25, 2023 09:00:00").getTime();
+
+setInterval(function seconds() {
+    var timeNow = new Date().getTime();
+
+    var timer = Math.floor((time - timeNow) / 1000);
+
+    var seconds = Math.floor(timer % 60);
+    var minutes = Math.floor(timer / 60);
+    var hours = Math.floor(minutes / 60);
+    var minutes = Math.floor(minutes % 60);
+
+    document.getElementById("timer").innerHTML = hours + " " + minutes + " " + seconds;
+}, 1000 )
+
+
